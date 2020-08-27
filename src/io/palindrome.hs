e71a892ff60a77153ebefb11cbff925f4df7352f
ryoheisato <thesugar87@gmail.com>
2020-08-27 23:30:11 +0900

    transformaation of input (interact function). related to 09/advanced_io.hs

---
+++ src/io/palindrome.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = interact respondPalindromes
+
+respondPalindromes :: String -> String
+respondPalindromes =
+    unlines .
+    map (\xs -> if isPal xs then "palindrome" else "not a palindrome") .
+    lines
+
+isPal :: String -> Bool
+isPal xs = xs == reverse xs
